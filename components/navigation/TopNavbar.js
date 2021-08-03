1ddd2956b73c8471f2e9c14462fa62346459cc8f
Daniel Otieno <danielotieno.ke@gmail.com>
2021-08-03 14:00:16 +0300

    chore:create top navbar component

---
+++ components/navigation/TopNavbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const TopNavbar = () => {
+  return (
+    <div className='top-header-area'>
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className='col-lg-8 col-md-9 col-sm-6'>
+            <ul className='header-content-left'>
+              <li>
+                <i className='bx bx-time'></i>
+                Mon-Fri 9am-5pm
+              </li>
+              <li>
+                <a href='tel:+822456974'>
+                  <i className='bx bx-phone-call'></i>
+                  Call Us: +353 85 850 0314
+                </a>
+              </li>
+              <li>
+                <a href='mailto:hello@info.com'>
+                  <i className='bx bxs-paper-plane'></i>
+                  Email: david.maguire@vpc-solutions.com
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='col-lg-4 col-md-3 col-sm-6'>
+            <ul className='header-content-right'>
+              <li>
+                <a href='#' target='_blank'>
+                  <i className='bx bxl-facebook'></i>
+                </a>
+              </li>
+              <li>
+                <a href='#' target='_blank'>
+                  <i className='bx bxl-twitter'></i>
+                </a>
+              </li>
+
+              <li>
+                <a href='#' target='_blank'>
+                  <i className='bx bxl-instagram'></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNavbar;
