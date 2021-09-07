73eb0208585584a2dc362b019aa8a1afe7404d25
Daniel Otieno <danielotieno.ke@gmail.com>
2021-09-07 16:51:24 +0300

    chore:add contact form component

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,6 @@
-const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://corf-react.envytheme.com' 
-: 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://vpc-solutions.com'
+    : 'http://localhost:3000';
 
 export default baseUrl;
