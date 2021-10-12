74d712f57cebe37e8b9ace59dd7aa25ece4bd6ce
Daniel Otieno <danielotieno.ke@gmail.com>
2021-10-12 17:26:29 +0300

    chore:refactor banner items

--- components/banner/MainBanner.js
+++ components/banner/MainBanner.js
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import React from 'react';
+import { bannerData } from '../constants/Banner/bannerData';
 
 const OwlCarousel = dynamic(import('react-owl-carousel3'));
 
@@ -29,133 +30,36 @@ const MainBanner = () => {
           <OwlCarousel
             className='hero-slider-wrap owl-carousel owl-theme'
             {...options}>
-            <div className='slider-item slider-item-bg-1'>
-              <div className='d-table'>
-                <div className='d-table-cell'>
-                  <div className='container'>
-                    <div className='row align-items-center'>
-                      <div className='col-lg-9'>
-                        <div className='slider-text one'>
-                          <span className='top-title'>
-                            Value Proposition Consulting
-                          </span>
-                          <h1>We architect intelligent IT strategies</h1>
-                          <p>
-                            We are a software development and consulting company
-                            that creates and transforms products and enterprises
-                            to drive growth.
-                          </p>
+            {bannerData.map((item, index) => {
+              return (
+                <div key={index} className={`slider-item ${item.imgLink}`}>
+                  <div className='d-table'>
+                    <div className='d-table-cell'>
+                      <div className='container'>
+                        <div className='row align-items-center'>
+                          <div className='col-lg-9'>
+                            <div className='slider-text one'>
+                              <span className='top-title'>
+                                Value Proposition Consulting
+                              </span>
+                              <h1>{item.title}</h1>
+                              <p>{item.description}</p>
 
-                          <div className='slider-btn'>
-                            <Link href='/contact-us'>
-                              <a className='default-btn'>Get In Touch</a>
-                            </Link>
+                              <div className='slider-btn'>
+                                <Link href='/contact-us'>
+                                  <a className='default-btn'>Get In Touch</a>
+                                </Link>
+                              </div>
+                            </div>
                           </div>
+                          <div className='col-lg-3'></div>
                         </div>
                       </div>
-
-                      <div className='col-lg-3'></div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-
-            <div className='slider-item slider-item-bg-2'>
-              <div className='d-table'>
-                <div className='d-table-cell'>
-                  <div className='container'>
-                    <div className='row align-items-center'>
-                      <div className='col-lg-9'>
-                        <div className='slider-text two'>
-                          <span className='top-title'>
-                            Value Proposition Consulting
-                          </span>
-                          <h1>Let Us Make Something Good Together</h1>
-                          <p>
-                            Using a tech-agnostic approach, we provide the right
-                            blend of software and hardware solutions that let
-                            you capture the full value of tech.
-                          </p>
-
-                          <div className='slider-btn'>
-                            <Link href='/contact-us'>
-                              <a className='default-btn'>Get In Touch</a>
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className='col-lg-3'></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='slider-item slider-item-bg-3'>
-              <div className='d-table'>
-                <div className='d-table-cell'>
-                  <div className='container'>
-                    <div className='row align-items-center'>
-                      <div className='col-lg-9'>
-                        <div className='slider-text one'>
-                          <span className='top-title'>
-                            Value Proposition Consulting
-                          </span>
-                          <h1>You are unique. So we are</h1>
-                          <p>
-                            We think in terms of value, not technology. We deep
-                            dive into your business to deliver innovations
-                            tailored to your business needs.
-                          </p>
-
-                          <div className='slider-btn'>
-                            <Link href='/contact-us'>
-                              <a className='default-btn'>Get In Touch</a>
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className='col-lg-3'></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className='slider-item slider-item-bg-4'>
-              <div className='d-table'>
-                <div className='d-table-cell'>
-                  <div className='container'>
-                    <div className='row align-items-center'>
-                      <div className='col-lg-9'>
-                        <div className='slider-text two'>
-                          <span className='top-title'>
-                            Value Proposition Consulting
-                          </span>
-                          <h1>You are unique. So we are</h1>
-                          <p>
-                            We think in terms of value, not technology. We deep
-                            dive into your business to deliver innovations
-                            tailored to your business needs.
-                          </p>
-
-                          <div className='slider-btn'>
-                            <Link href='/contact-us'>
-                              <a className='default-btn'>Get In Touch</a>
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className='col-lg-3'></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </OwlCarousel>
         ) : (
           ''
---
+++ components/constants/Banner/bannerData.js
@@ -0,0 +1,26 @@
+export const bannerData = [
+  {
+    imgLink: 'slider-item-bg-1',
+    title: 'We architect intelligent IT strategies',
+    description:
+      'We are a software development and consulting company that creates and transforms products and enterprises to drive growth.',
+  },
+  {
+    imgLink: 'slider-item-bg-2',
+    title: 'Let Us Make Something Good Together',
+    description:
+      'Using a tech-agnostic approach, we provide the right blend of software and hardware solutions that let you capture the full value of tech.',
+  },
+  {
+    imgLink: 'slider-item-bg-3',
+    title: 'You are unique. So we are',
+    description:
+      'We think in terms of value, not technology. We deep dive into your business to deliver innovations tailored to your business needs.',
+  },
+  {
+    imgLink: 'slider-item-bg-4',
+    title: 'You are unique. So we are',
+    description:
+      'We think in terms of value, not technology. We deep dive into your business to deliver innovations tailored to your business needs.',
+  },
+];
