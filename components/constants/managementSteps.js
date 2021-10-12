6c259137a626d330f3454be5bb28296dc8bec3e5
Daniel Otieno <danielotieno.ke@gmail.com>
2021-10-12 11:56:21 +0300

    chore:refactor management steps

---
+++ components/constants/managementSteps.js
@@ -0,0 +1,26 @@
+export const managementSteps = [
+  {
+    uuid: 'a',
+    title: 'Step 1: Ingestion',
+    description:
+      'Collecting internal and external data from available databases, file repositories, streaming APIs, and other sources.',
+  },
+  {
+    uuid: 'b',
+    title: 'Step 2: Structuring',
+    description:
+      ' Administering, versioning, cleansing, standardizing, correcting, and validating the integrated data.',
+  },
+  {
+    uuid: 'c',
+    title: 'Step 3: Contextualization & Enrichment',
+    description:
+      'Identifying and removing duplicates and, if needed, bringing in more data sources to restore missing information.',
+  },
+  {
+    uuid: 'd',
+    title: 'Step 4: Warehousing',
+    description:
+      'Having your data stored in a secure environment with easy querying, fast retrieval, and prompt response times.',
+  },
+];
--- components/management/ManagementSteps.js
+++ components/management/ManagementSteps.js
@@ -1,6 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
-import Fade from 'react-reveal/Fade';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import { managementSteps } from '../constants/managementSteps';
 
 const ManagementSteps = () => {
   return (
@@ -33,91 +40,24 @@ const ManagementSteps = () => {
           </div>
 
           <div className='col-lg-6'>
-            <ul>
-              <p>
-                <Fade left>
-                  <a
-                    className='default-btn w-100'
-                    data-bs-toggle='collapse'
-                    href='#collapseExample1'
-                    role='button'
-                    aria-expanded='false'
-                    aria-controls='collapseExample1'>
-                    1. Ingestion
-                  </a>
-                </Fade>
-
-                <div className='collapse pt-10' id='collapseExample1'>
-                  <div className='card card-body bg-light border'>
-                    Collecting internal and external data from available
-                    databases, file repositories, streaming APIs, and other
-                    sources.
-                  </div>
-                </div>
-              </p>
-
-              <p>
-                <Fade right>
-                  <a
-                    className='default-btn w-100'
-                    data-bs-toggle='collapse'
-                    href='#collapseExample2'
-                    role='button'
-                    aria-expanded='false'
-                    aria-controls='collapseExample2'>
-                    2. Structuring
-                  </a>
-                </Fade>
-
-                <div className='collapse pt-10' id='collapseExample2'>
-                  <div className='card card-body bg-light border'>
-                    Administering, versioning, cleansing, standardizing,
-                    correcting, and validating the integrated data.
-                  </div>
-                </div>
-              </p>
-
-              <p>
-                <Fade top>
-                  <a
-                    className='default-btn w-100'
-                    data-bs-toggle='collapse'
-                    href='#collapseExample4'
-                    role='button'
-                    aria-expanded='false'
-                    aria-controls='collapseExample4'>
-                    3. Contextualization and enrichment
-                  </a>
-                </Fade>
-
-                <div className='collapse pt-10' id='collapseExample4'>
-                  <div className='card card-body bg-light border'>
-                    Identifying and removing duplicates and, if needed, bringing
-                    in more data sources to restore missing information.
-                  </div>
-                </div>
-              </p>
-              <p>
-                <Fade bottom>
-                  <a
-                    className='default-btn w-100'
-                    data-bs-toggle='collapse'
-                    href='#collapseExample5'
-                    role='button'
-                    aria-expanded='false'
-                    aria-controls='collapseExample5'>
-                    4. Warehousing
-                  </a>
-                </Fade>
-
-                <div className='collapse pt-10' id='collapseExample5'>
-                  <div className='card card-body bg-light border'>
-                    Having your data stored in a secure environment with easy
-                    querying, fast retrieval, and prompt response times.
-                  </div>
-                </div>
-              </p>
-            </ul>
+            <div className='faq-accordion'>
+              <Accordion allowZeroExpanded>
+                {managementSteps.map((item, index) => {
+                  return (
+                    <AccordionItem key={index} uuid={item.uuid}>
+                      <AccordionItemHeading>
+                        <AccordionItemButton className='management-faq-btn'>
+                          <span>{item.title}</span>
+                        </AccordionItemButton>
+                      </AccordionItemHeading>
+                      <AccordionItemPanel>
+                        <p>{item.description}</p>
+                      </AccordionItemPanel>
+                    </AccordionItem>
+                  );
+                })}
+              </Accordion>
+            </div>
           </div>
         </div>
       </div>
